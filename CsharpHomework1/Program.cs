2fa3a81d0b5b7177a960ac15ab4023fff6e4aff2
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-11-22 16:39:29 +0300

    fourth task comlete

--- CsharpHomework1/Program.cs
+++ CsharpHomework1/Program.cs
@@ -64,18 +64,32 @@
 // -3 -> нет
 // 7 -> нет
 
-Console.Write("Введите число ");
-int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите число ");
+// int a = int.Parse(Console.ReadLine());
 
-if(a%2 < 1)
-{
-    Console.Write($"{a} - число четное");
-}
-else
-{
-    Console.Write($"{a} - число не четное");
-}
+// if(a%2 < 1)
+// {
+//     Console.Write($"{a} - число четное");
+// }
+// else
+// {
+//     Console.Write($"{a} - число не четное");
+// }
 // Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
 
 // 5 -> 2, 4
 // 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число ");
+int N = int.Parse(Console.ReadLine());
+
+int Nmin = 1;
+
+while(N>=Nmin)
+{
+    if(N%2 < 1)
+        {
+            Console.Write($"{N}, ");
+        }          
+    N = N-1;
+}
