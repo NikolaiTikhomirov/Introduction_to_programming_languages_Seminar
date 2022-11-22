b636200d646be9809de4653032a87ffd279c9794
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-11-22 16:14:52 +0300

    second task comlete

--- CsharpHomework1/Program.cs
+++ CsharpHomework1/Program.cs
@@ -4,32 +4,59 @@
 // a = 2 b = 10 -> max = 10
 // a = -9 b = -3 -> max = -3
 
-Console.Write("Введите первое число ");
-int a = int.Parse(Console.ReadLine());
-Console.Write("Введите второе число ");
-int b = int.Parse(Console.ReadLine());
+// Console.Write("Введите первое число ");
+// int a = int.Parse(Console.ReadLine());
+// Console.Write("Введите второе число ");
+// int b = int.Parse(Console.ReadLine());
 
-if (a > b)
-{
-    Console.WriteLine("Первое число больше второго");
-}
-else
-{
-    if (b > a)
-    {
-        Console.WriteLine("Второе число больше первого");
-    }
-    else
-    {
-        Console.WriteLine("Первое число равно второму");
-    }
-}
+// if (a > b)
+// {
+//     Console.WriteLine("Первое число больше второго");
+// }
+// else
+// {
+//     if (b > a)
+//     {
+//         Console.WriteLine("Второе число больше первого");
+//     }
+//     else
+//     {
+//         Console.WriteLine("Первое число равно второму");
+//     }
+// }
 
 // Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
 // 2, 3, 7 -> 7
 // 44 5 78 -> 78
 // 22 3 9 -> 22
+
+Console.Write("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число ");
+int c = int.Parse(Console.ReadLine());
+
+int maxnum = a;
+
+if (b > maxnum)
+{
+    maxnum = b;
+    if (c > maxnum)
+    {
+        maxnum = c;
+    }
+}
+else
+{
+    if (c > maxnum)
+    {
+        maxnum = c;
+    }
+}
+Console.WriteLine($"Максимальное число {maxnum}");
+
 
 // Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
