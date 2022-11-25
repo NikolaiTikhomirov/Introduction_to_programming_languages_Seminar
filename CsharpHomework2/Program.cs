112874033f9ed196e3abcf60c58acaaf1b965d4c
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-11-25 22:35:41 +0300

    task 10 complete

--- CsharpHomework2/Program.cs
+++ CsharpHomework2/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+var phrase = "Введите трёхзначное число ";
+int someNumber = 0;
+
+while(someNumber>1000 || someNumber<100)
+{
+    Console.WriteLine(phrase);
+    someNumber = int.Parse(Console.ReadLine());
+    phrase = "Введенное число вне диапазона! Введите трехзначное число ";
+}
+int secondNumberOfSomeNumber = someNumber/10%10;
+Console.WriteLine(secondNumberOfSomeNumber);
+
+
+
+// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+// 645 -> 5
+
+// 78 -> третьей цифры нет
+
+// 32679 -> 6
+
+// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
