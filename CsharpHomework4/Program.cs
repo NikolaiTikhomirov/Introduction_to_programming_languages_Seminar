605ab6104f53affd490fa429506c097f7b952574
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-12-03 22:43:01 +0300

    task 27 about sum complete

--- CsharpHomework4/Program.cs
+++ CsharpHomework4/Program.cs
@@ -3,23 +3,23 @@
 // Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
 // Не использовать Math.Pow()
 
-Console.WriteLine("Введитте два числа:");
-int number1 = int.Parse(Console.ReadLine());
-int number2 = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введитте два числа:");
+// int number1 = int.Parse(Console.ReadLine());
+// int number2 = int.Parse(Console.ReadLine());
 
-Console.WriteLine(Exponentiation(number1, number2));
+// Console.WriteLine(Exponentiation(number1, number2));
 
-int Exponentiation(int a, int b)
-{
-    int c = a;
-    int index = 1;
-    while(index < b)
-    {
-        c = c*a;
-        index++;
-    }
-    return c;
-}
+// int Exponentiation(int a, int b)
+// {
+//     int c = a;
+//     int index = 1;
+//     while(index < b)
+//     {
+//         c = c*a;
+//         index++;
+//     }
+//     return c;
+// }
 
 
 
@@ -29,6 +29,23 @@ int Exponentiation(int a, int b)
 // 452 -> 11
 // 82 -> 10
 // 9012 -> 12
+
+Console.Write("Введите число:");
+int someNumber = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SumOfSomeNumber(someNumber));
+
+int SumOfSomeNumber(int temp)
+{
+    int a = 0;
+    while(temp > 0)
+    {
+        a = a + temp % 10;
+        temp = temp / 10;
+    }
+    return a;
+}
+
 
 
 
