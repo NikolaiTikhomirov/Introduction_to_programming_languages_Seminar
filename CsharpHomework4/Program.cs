870b3fdb968416e90a1a798fd3ca30aefa1519ad
Nikolay Tikhomirov <79159489230@yandex.ru>
2022-12-05 12:13:31 +0300

    task 25 about Exponentiation upgraded

--- CsharpHomework4/Program.cs
+++ CsharpHomework4/Program.cs
@@ -3,44 +3,31 @@
 // Задание должно быть выполнено в методе. В методе не должно быть вывода в консоль.
 // Не использовать Math.Pow()
 
-// Console.WriteLine("Введитте два числа:");
-// int number1 = int.Parse(Console.ReadLine());
-// int number2 = int.Parse(Console.ReadLine());
+Console.WriteLine("Введитте два числа:");
+int number1 = int.Parse(Console.ReadLine());
+int number2 = int.Parse(Console.ReadLine());
 
-// if (number1 == 0 && number2 == 0) Console.WriteLine("Возводить 0 в степень 0 не имеет смысла, так как такое выражение не может быть определено.");
-// else Console.WriteLine(Exponentiation(number1, number2));
+if (number1 == 0 && number2 == 0) Console.WriteLine("Возводить 0 в степень 0 не имеет смысла, так как такое выражение не может быть определено.");
+else Console.WriteLine(Exponentiation(number1, number2));
 
-// decimal Exponentiation(int a, int b)
-// {
-//     decimal c = a;
-//     if (b == 0)
-//     {
-//         c = 1;
-//     }
-//     else
-//     {
-//         int index = 1;
-//         if (b < 0)
-//         {
-//             c = 1 / HalfExponentiation(a, -b);
-//         }
-//         else
-//         {
-//             c = HalfExponentiation(a, b);
-//         }
-//         decimal HalfExponentiation(int aa, int bb)
-//         {
-//             decimal cc = aa;
-//             while (index < bb)
-//             {
-//                 cc = cc * aa;
-//                 index++;
-//             }
-//             return cc;
-//         }
-//     }
-//     return c;
-// }
+decimal Exponentiation(int a, int b)
+{
+    decimal c = a;
+    if (b == 0) return 1;
+    int index = 1;
+    if (b < 0) return 1 / HalfExponentiation(a, -b);
+    return HalfExponentiation(a, b);
+    decimal HalfExponentiation(int aa, int bb)
+    {
+        decimal cc = aa;
+        while (index < bb)
+        {
+            cc = cc * aa;
+            index++;
+        }
+        return cc;
+    }
+}
 
 
 
@@ -75,28 +62,28 @@
 // 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
 // 6, 1, 33 -> [6, 1, 33]
 
-Console.WriteLine("Какой длины будет массив?");
-int arrLentgth = int.Parse(Console.ReadLine());
-int[] Array = new int[arrLentgth];
-int index = 0;
-while (index < arrLentgth)
-{
-    Console.Write($"Введите {index + 1} число: ");
-    Array[index] = int.Parse(Console.ReadLine());
-    index++;
-}
+// Console.WriteLine("Какой длины будет массив?");
+// int arrLentgth = int.Parse(Console.ReadLine());
+// int[] Array = new int[arrLentgth];
+// int index = 0;
+// while (index < arrLentgth)
+// {
+//     Console.Write($"Введите {index + 1} число: ");
+//     Array[index] = int.Parse(Console.ReadLine());
+//     index++;
+// }
 
-PrintArray(Array);
+// PrintArray(Array);
 
-void PrintArray(int[] Col)
-{
-    int count = Col.Length;
-    int position = 0;
-    Console.Write("[");
-    while (position < count - 1)
-    {
-        Console.Write($"{Col[position]}, ");
-        position++;
-    }
-    Console.Write($"{Col[Col.Length - 1]}]");
-}
+// void PrintArray(int[] Col)
+// {
+//     int count = Col.Length;
+//     int position = 0;
+//     Console.Write("[");
+//     while (position < count - 1)
+//     {
+//         Console.Write($"{Col[position]}, ");
+//         position++;
+//     }
+//     Console.Write($"{Col[Col.Length - 1]}]");
+// }
